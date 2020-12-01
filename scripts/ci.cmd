d1b06f830029002ab626b17da9547c42d517f0a8
Nick Felker <fleker@google.com>
2020-12-01 18:06:22 -0500

    Add equivalent script for Windows execute/test
    Bug: 174267320
    Change-Id: Ie3ca4521418c9a7a653a20cd55b2f904524bf616

---
+++ scripts/ci.cmd
@@ -0,0 +1,19 @@
+rem CI script
+
+rem Install Bazel using Choco
+echo "Installing/updating bazel"
+choco install bazel -y
+
+rem Use placeholder secret file
+mkdir data\
+echo '
+    {"installed":{"client_id":"PLACEHOLDER.apps.googleusercontent.com","project_id":"PLACEHOLDER","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"PLACEHOLDER","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+' > data\client_not_so_secret.json
+
+rem Ensure that our code builds
+bazel build gactions
+rem Print version
+bazel run gactions -- version
+
+rem Run all tests
+bazel test ...
